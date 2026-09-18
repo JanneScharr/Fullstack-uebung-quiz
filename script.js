1ffd7d07fa4af150b91bfccaa03b69900dffae89
janne.n.scharr <janne.n.scharr@accenture.com>
2026-09-18 13:09:13 +0200

    erste quiz Basis

--- script.js
+++ script.js
@@ -1,1 +1,176 @@
-console.log("Hello World");
+// // //console.log("Hello World");
+// // //let Zahl = 5
+// // //console.log (typeof Zahl);
+
+// // let number = 26 // Number 
+// // let text = "JS ist müll" // String
+// // let bestanden = true; // boolean
+
+// // console.log("Ich bin " + number + " Jahre alt.")
+// // console.log("Und ich finde " + text)
+// // console.log(bestanden)
+
+// // number = 19;
+// // text = "Vielleicht hab ichs doch verstanden" 
+// // bestanden = false
+
+// // console.log("Ich bin " + number + " Jahre alt.")
+// // console.log("Und " + text)
+// // console.log(bestanden)
+
+// // let zahl1 = 5;
+// // let zahl2= 10; 
+
+// // console.log(zahl1 + zahl2);
+// // console.log(zahl1 - zahl2);
+// // console.log(zahl1 * zahl2);
+// // console.log(zahl1 / zahl2);
+// // console.log(zahl1);
+// // zahl1++;
+// // console.log(zahl1);
+// // zahl1 = zahl1 + 1;
+// // console.log(zahl1);
+
+
+// // zahl1 = zahl1 + zahl2;
+// // zahl1 += 5;
+// // console.log(zahl1);
+
+
+// // let firstName = "Janne";
+// // let lastName = "Scharr";
+
+// // let fullName = firstName + " " + lastName;
+
+// // console.log(fullName);
+
+// // console.log(5 == "5");
+// // console.log(5 === "5");
+// // console.log(5 != "5");
+// // console.log(5 <= "5");
+// // console.log(5 > "5");
+
+// // console.log("Alpha" < "Beta");
+
+
+// // console.log("Alpha" < "Beta" && "Beta" < "Gamma");
+
+
+// // function functionName(zahl1,zahl2){ 
+
+// //     let result = zahl1 + zahl2;
+// //     return result;
+
+
+// // }
+
+// // console.log(functionName(5, 5));
+
+// // let ergebnis = functionName(3, 8);
+
+// // let ergebniss = functionName(67, 67);
+
+// // console.log(ergebnis);
+// // console.log(ergebniss);
+
+
+// // conditions:
+
+// // zahl = 0.3;
+
+// // if (zahl < 0,5) {
+// // zahl = 0; 
+// // }
+// // else {
+// //     zahl = 1;
+// // }
+
+// // console.log(zahl);
+
+// // let zahl = 5;
+
+// // if(zahl == 5) {
+// // console.log("Die Zahl ist eine 5.");
+// // }
+
+// // else{
+// //     console.log("Die Zahl ist nicht 5.");
+// // }
+
+// // let istDurstig = true;
+// // let willKaffee = true; 
+// // if (!istDurstig) { // Aurufezeichen dreht fkt. um, jetzt wirds nur ausgeführt wenns falsch ist
+// //     if (willKaffee){
+// //          // Kaffee kochen
+// //     }
+// // }
+
+
+// let zahl1 = "5";
+// let zahl2 = "3";
+
+
+// // function compareNumbers(zahl1,zahl2) {
+// //     if (zahl1 > zahl){
+// //         return = zahl1;
+// //     } else{
+// //         return zahl2;
+// //     }
+// // }
+
+// // result: compareNumbers (zahl1,zahl2);        
+// // console.log (result);
+
+
+// // if (zahl1 == zahl2);
+// // console.log ("gleich")
+
+// // let difficulty = 3; 
+// // let text; 
+
+
+// // switch(difficulty){
+
+// //     case 1: difficulty = "easy";
+// //         break
+    
+// //     case 2: difficulty = "medium";
+// //         break
+
+// //     case 3: difficulty = "hard";
+// //         break
+// // }
+// // console.log(difficulty);
+
+// // let canZombiesSmashDoors = "true"
+
+// // canZombiesSmashDoors = ( difficulty == "hard") ? true: false; 
+ 
+
+// // for (let i = 0; i < 5; i++) {
+// //     console.log (i);
+// // }
+
+// // let zahl = 1;
+
+// // while (zahl < 1000) {
+// //     zahl = zahl * 2; 
+// //     console.log(zahl);
+// // }
+// let zahl = 0;
+
+// // for (let i = 0; i < 11; i++){
+// //     console.log(i);
+// // }
+
+// // while (zahl < 10) {
+// //     zahl = zahl + 1;
+// //     console.log(zahl)
+// // }
+
+// do {
+//     zahl++; 
+
+// while(zahl < 10);
+// console.log(zahl);
+// } 
---
+++ script2.js
@@ -0,0 +1,108 @@
+// Frage 1
+const frage1a = document.getElementById("frage1-a");
+const frage1b = document.getElementById("frage1-b");
+const frage1c = document.getElementById("frage1-c");    
+
+const ergebnis1 = document.getElementById("ergebnis1");
+
+frage1a.addEventListener("click", function() {
+    ergebnis1.textContent = "Richtig!";
+    });
+
+frage1b.addEventListener("click", function() {
+    ergebnis1.textContent = "Falsch!";
+    });
+
+frage1c.addEventListener("click", function() {
+    ergebnis1.textContent = "Falsch!";
+    }); 
+
+// Frage 2
+const frage2a = document.getElementById("frage2-a");
+const frage2b = document.getElementById("frage2-b");
+const frage2c = document.getElementById("frage2-c");
+const ergebnis2 = document.getElementById("ergebnis2");
+
+frage2a.addEventListener("click", function() {
+    ergebnis2.textContent = "Richtig!";
+    });
+
+frage2b.addEventListener("click", function() {
+    ergebnis2.textContent = "Falsch!";
+    }  );
+
+frage2c.addEventListener("click", function() {
+    ergebnis2.textContent = "Falsch!";
+    });     
+
+// Frage 3
+const frage3a = document.getElementById("frage3-a");
+const frage3b = document.getElementById("frage3-b");
+const frage3c = document.getElementById("frage3-c");
+const ergebnis3 = document.getElementById("ergebnis3"); 
+
+frage3a.addEventListener("click", function() {
+    ergebnis3.textContent = "Richtig!";
+    });
+
+frage3b.addEventListener("click", function() {
+    ergebnis3.textContent = "Falsch!";
+    }  );
+
+frage3c.addEventListener("click", function() {
+    ergebnis3.textContent = "Falsch!";
+    });
+
+// Frage 4
+const frage4a = document.getElementById("frage4-a");
+const frage4b = document.getElementById("frage4-b");
+const frage4c = document.getElementById("frage4-c");
+const ergebnis4 = document.getElementById("ergebnis4"); 
+
+frage4a.addEventListener("click", function() {
+    ergebnis4.textContent = "Richtig!";
+    });
+
+frage4b.addEventListener("click", function() {
+    ergebnis4.textContent = "Falsch!";
+    }  );
+
+frage4c.addEventListener("click", function() {
+    ergebnis4.textContent = "Falsch!";
+    });
+
+// Frage 5
+const frage5a = document.getElementById("frage5-a");
+const frage5b = document.getElementById("frage5-b");
+const frage5c = document.getElementById("frage5-c");
+const ergebnis5 = document.getElementById("ergebnis5"); 
+
+frage5a.addEventListener("click", function() {
+    ergebnis5.textContent = "Falsch!";
+    });
+
+frage5b.addEventListener("click", function() {
+    ergebnis5.textContent = "Richtig!";
+    }  );
+
+frage5c.addEventListener("click", function() {
+    ergebnis5.textContent = "Falsch!";
+    });
+
+// Frage 6
+const frage6a = document.getElementById("frage6-a");
+const frage6b = document.getElementById("frage6-b");
+const frage6c = document.getElementById("frage6-c");
+const ergebnis6 = document.getElementById("ergebnis6"); 
+
+frage6a.addEventListener("click", function() {
+    ergebnis6.textContent = "Richtig!";
+    });
+
+frage6b.addEventListener("click", function() {
+    ergebnis6.textContent = "Falsch!";
+    }  );
+
+frage6c.addEventListener("click", function() {
+    ergebnis6.textContent = "Falsch!";
+    });
